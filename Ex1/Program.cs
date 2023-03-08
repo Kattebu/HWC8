847d67c0e736f625634b72553281b854701f94e4
Kattebu <butskoev@tut.by>
2023-03-08 21:28:56 +0300

    change

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] GetMatrix(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] matrix=new int[rows,cols];
+    for(int i=0;i<matrix.GetLength(0);i++)
+    {
+        for(int j=0;j<matrix.GetLength(1);j++)
+        {
+            matrix[i,j]=new Random().Next(minValue,maxValue+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,]matr)
+{
+    for(int i=0;i<matr.GetLength(0);i++)
+    {
+        for(int j=0;j<matr.GetLength(1);j++)
+        {
+            Console.Write(matr[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] resultMatrix=GetMatrix(5,6,0,10);
+PrintMatrix(resultMatrix);
+
+for(int i=0;i<resultMatrix.GetLength(0);i++)
+{
+    for(int j=0;j<resultMatrix.GetLength(1);j++)
+    {
+        int temp=0;
+        if()
+    }
+}
